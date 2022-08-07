a342077d605684e7a9cf256fb0eea4b7a6b18ffd
MXXYT <110575970+MXXYT@users.noreply.github.com>
2022-08-07 17:39:12 -0400

    flag host done

---
+++ src/ReportsToSupport/identifyCommercialHost.java
@@ -0,0 +1,59 @@
+package ReportsToSupport;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.Statement;
+import java.util.Scanner;
+
+public class identifyCommercialHost {
+    public static void main(String[] args) {
+
+        try {
+            String url = "jdbc:mysql://localhost:3306/C43Project";
+            Connection conn = DriverManager.getConnection(url, "root", "");
+            Statement st = conn.createStatement();
+
+            ResultSet count = st.executeQuery("SELECT COUNT(lists.lid) FROM lists " +
+                                            "INNER JOIN owns ON owns.lid=lists.lid WHERE lists.status=1 "  +
+                                            "GROUP BY lists.country, owns.uid ORDER BY lists.country, lists.city, owns.uid, COUNT(lists.lid) DESC");
+            double row = 0;
+            double total = 0;
+            while (count.next()){
+                total += count.getInt(1);
+                row += 1;
+            }
+            double avg = total/row;
+            double flag = 1.1 * avg;
+
+            System.out.println("The following hosts are considered to be flagged since their number of listings are greater than flag value: " + flag);
+
+            ResultSet resultSet = st.executeQuery("SELECT owns.uid, COUNT(lists.lid) FROM lists " +
+                    "INNER JOIN owns ON owns.lid=lists.lid WHERE lists.status=1 "  +
+                    "GROUP BY lists.country, owns.uid HAVING COUNT(lists.lid) > '" + flag + "' ORDER BY lists.country, lists.city, owns.uid, COUNT(lists.lid) DESC");
+
+            System.out.println("Host ID  Number of Listings");
+            while (resultSet.next()){
+                System.out.println(resultSet.getInt(1) + "\t\t" + resultSet.getInt(2));
+            }
+            conn.close();
+        } catch (Exception e) {
+            System.err.println("Got an exception! ");
+            System.err.println(e.getMessage());
+        }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    }
+}
