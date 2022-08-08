7e926f04cada9f14b7959fc0e6160c2a69b39c6e
Hongxiao <hongxiao.niu@mail.utoronto.ca>
2022-08-08 04:47:20 -0400

    Did popular Phrases

--- src/HostToolkit/temp.java
+++
@@ -1,4 +0,0 @@
-package HostToolkit;
-
-public class temp {
-}
--- src/ReportsToSupport/popularPhrases.java
+++ src/ReportsToSupport/popularPhrases.java
@@ -1,5 +1,39 @@
 package ReportsToSupport;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.Statement;
+import java.util.Scanner;
+
 public class popularPhrases {
-    
+    public static void main(String[] args) {
+        System.out.println("-----------------------------------------------------");
+        System.out.println("Welcome to AirBNB!\nThis function is for showing the most popular word in a single list.");
+        System.out.println("-----------------------------------------------------\n");
+
+        try {
+            String url = "jdbc:mysql://localhost:3306/C43Project";
+            Connection conn = DriverManager.getConnection(url, "root", "");
+            Statement st = conn.createStatement();
+
+            Scanner scInt = new Scanner(System.in);
+            System.out.println("What list that you want to see the most popular word?");
+            int lid = scInt.nextInt();
+            
+            // Check renter is in users
+            ResultSet checkLid = st.executeQuery("SELECT * FROM lists WHERE lid='" + lid + "' AND status=1");
+            if (!checkLid.next()) {
+                System.out.println("ERROR: the lid: " + lid + ", you enter are not exist! please check the sin is correct");
+                return;
+            }
+
+        } catch (Exception e) {
+            System.err.println("Got an exception! ");
+            System.err.println(e.getMessage());
+        }
+
+
+
+    }
 }
